a1e3808b49fbc514698037d8f267831f8c5ffa6a
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-02-20 17:09:47 -0500

    creando recurso de pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+      }),
+    PokemonModule
+  ],
   controllers: [],
   providers: [],
 })
