5a7a2e96ca9b06b7a1adf4d2c163944e63b23b8e
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-02-20 19:28:12 -0500

    conexion a la base de datos

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
 
+
 @Module({
   controllers: [PokemonController],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/test')
+  ],
   providers: [PokemonService]
 })
-export class PokemonModule {}
+export class PokemonModule { }
