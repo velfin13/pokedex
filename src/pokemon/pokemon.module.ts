25f59ccd0edfe5ae4f526959cf4d4724f66a946e
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-02-20 19:51:30 -0500

    schema de base de datos

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 import { PokemonController } from './pokemon.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pokemon, PokemonSchema } from './entities/pokemon.entity';
 
 
 @Module({
   controllers: [PokemonController],
-  imports: [],
+  imports: [MongooseModule.forFeature([
+    {
+      name: Pokemon.name,
+      schema: PokemonSchema
+    }
+  ])],
   providers: [PokemonService]
 })
 export class PokemonModule { }
