e8f774a54838837b872e9d1dd044286d0c42c152
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-02-20 21:03:25 -0500

    validando el post

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePokemonDto {}
+import { IsNotEmpty, IsNumber, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreatePokemonDto {
+    @IsString()
+    @MinLength(1)
+    @IsNotEmpty()
+    name: string;
+    @IsNumber()
+    @IsPositive()
+    @IsNotEmpty()
+    @Min(1)
+    no: number;
+}
