f71d62a7167b655be0485d966c0fbb39210d2ca0
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-02-25 15:58:31 -0500

    update

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -18,7 +18,7 @@ export class SeedService {
 
   async executeSeed() {
     await this.pokemonModel.deleteMany({});
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=350');
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
     const pokemonToInsert: { name: string, no: number }[] = [];
     data.results.forEach(({ name, url }) => {
