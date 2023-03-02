5268f22863c61b5047675b0da33cc1353fb02266
Velfin Velasquez <velfinvelasquez@gmail.com>
2023-03-01 21:00:18 -0500

    variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,15 @@ import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/test'),
+    MongooseModule.forRoot(process.env.MONGO_URL),
     PokemonModule,
     CommonModule,
     SeedModule
@@ -19,4 +21,9 @@ import { SeedModule } from './seed/seed.module';
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    console.log(process.env);
+    
+  }
+ }
